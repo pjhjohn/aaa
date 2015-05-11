4a971a2000796ea3b260e7686a5fcffe049617bb
pjhjohn <pjhjohn@gmail.com>
2015-05-11 14:24:44 +0900

    Indentation using spaces

--- ga.cpp
+++ ga.cpp
@@ -6,8 +6,8 @@
 #include<fstream>
 
 /* PROJECT 2 ESSENTIALS */
-#define MAXN	600 // Maximum value of N ( of project 2 )
-#define PSIZE	10  // Size of the population
+#define MAXN    600 // Maximum value of N ( of project 2 )
+#define PSIZE   10  // Size of the population
 
 double X[MAXN], Y[MAXN];    // (X[i], Y[i]) := the i-th location
 int N;                      // The number of locations
@@ -15,35 +15,35 @@ long long TimeLimit;        // Time Limitation
 double Dist[MAXN][MAXN];    // Distance Map
 
 typedef struct {            // Note that the representation is currently order-based.
-	int ch[MAXN];           // chromosome : a permutation of (0, 1, ..., N-1).
-	double f;               // fitness
+    int ch[MAXN];           // chromosome : a permutation of (0, 1, ..., N-1).
+    double f;               // fitness
 } SOL;
 SOL population[PSIZE];      // Population
 SOL record;                 // Best solution ( eval() automatically updates this )
 
 double eval(SOL *s) {       // calculate the fitness of s and store it into s->f
-	s->f = 0;
-	for (int i = 0; i < N; i++) s->f += Dist[s->ch[i]][s->ch[(i+1)%N]];
-	if (record.f > s->f) {
-		record.f = s->f;
-		for (int i = 0; i < N; i++) record.ch[i] = s->ch[i];
-	} return s->f;
+    s->f = 0;
+    for (int i = 0; i < N; i++) s->f += Dist[s->ch[i]][s->ch[(i+1)%N]];
+    if (record.f > s->f) {
+        record.f = s->f;
+        for (int i = 0; i < N; i++) record.ch[i] = s->ch[i];
+    } return s->f;
 }
 
 void gen_rand_sol(SOL *s) { // generate a random order-based solution at s
     for (int i = 0; i < N; i++) s->ch[i] = i;
-	for (int i = 0; i < N; i++) {
-		int r = i + rand() % (N - i);	// r is a random number in [i..N-i)
-		int tmp = s->ch[i]; s->ch[i] = s->ch[r]; s->ch[r] = tmp; // swap
-	} eval(s);
+    for (int i = 0; i < N; i++) {
+        int r = i + rand() % (N - i);   // r is a random number in [i..N-i)
+        int tmp = s->ch[i]; s->ch[i] = s->ch[r]; s->ch[r] = tmp; // swap
+    } eval(s);
 }
 
 
 void pprint(const char* TAG, const SOL *s) {
   printf("<%s>\n", TAG);
   for(int i = 0; i < N; i ++) {
-	  printf("%3d ", s->ch[i]);
-	  if(i%10==9) printf("\n");
+      printf("%3d ", s->ch[i]);
+      if(i%10==9) printf("\n");
   } printf("%lf\n", s->f);
 }
 
@@ -77,7 +77,7 @@ void RouletteWheelSelection(SOL **p) {
 
 /* Random Selection */
 void randomSelection(SOL **p) {
-	*p = &population[rand() % PSIZE];
+    *p = &population[rand() % PSIZE];
 }
 
 // choose one solution from the population
@@ -111,8 +111,8 @@ int isOkayToAppend(const int index, const int *ch) {
 int isOffspringCompleted(const SOL *offspring) {
   int sum = 0;
   for(int i = 0; i < N; i ++) {
-	  if(offspring->ch[i] == -1) return 0;
-	  else sum += offspring->ch[i];
+      if(offspring->ch[i] == -1) return 0;
+      else sum += offspring->ch[i];
   }
   return sum == N * (N-1) / 2;
 }
@@ -132,20 +132,20 @@ void PMXCrossover(const SOL *p1, const SOL *p2, SOL *c, const int stackTrace) {
       c->ch[current] = p2->ch[current];
       current = (current+1)%N;
       remains --;
-	  if(stackTrace) pprint("after safe", c);
+      if(stackTrace) pprint("after safe", c);
     } else {
       int appended = 0;
       while(!appended) {
         if(hasDuplication(p2->ch[j], p1->ch, s1, s2)) {
-			if(stackTrace) printf("collision. passing @ %d\n", j);
-			j = (j+1)%N;
-		}
+            if(stackTrace) printf("collision. passing @ %d\n", j);
+            j = (j+1)%N;
+        }
         else {
           c->ch[current] = p2->ch[j];
           current = (current+1)%N;
           appended = 1;
           remains --;
-		  if(stackTrace) { printf("takes # @ %d\n", j); pprint("after notsafe", c); }
+          if(stackTrace) { printf("takes # @ %d\n", j); pprint("after notsafe", c); }
           j = (j+1)%N;
         }
       }
@@ -155,38 +155,38 @@ void PMXCrossover(const SOL *p1, const SOL *p2, SOL *c, const int stackTrace) {
     return;
   } else {
     if(stackTrace) exit(1);
-	else {
-		PMXCrossover(p1, p2, c, 1);
-		exit(1);
-	}
+    else {
+        PMXCrossover(p1, p2, c, 1);
+        exit(1);
+    }
   }
 }
 int duplicated(const int val, const int *ch, int from, int to) {
-	int i;
-	for(i = from; i < to; i ++) if (val == ch[i]) return 1;
-	return 0;
+    int i;
+    for(i = from; i < to; i ++) if (val == ch[i]) return 1;
+    return 0;
 }
 void OrderCrossover(const SOL *p1, const SOL *p2, SOL *c) {
-	setRandomSliceIndexes();
-	int i;
-	for(i = 0; i < N; i++) c->ch[i] = -1;
-	for(i = s1; i < s2; i++) c->ch[i] = p1->ch[i];
+    setRandomSliceIndexes();
+    int i;
+    for(i = 0; i < N; i++) c->ch[i] = -1;
+    for(i = s1; i < s2; i++) c->ch[i] = p1->ch[i];
 
-	int iC = 0, iP2 = 0;
-	while (iP2 < N) {
-		if(duplicated(p2->ch[iP2], p1->ch, s1, s2)) {
-			iP2 ++;
-		} else {
-			c->ch[iC] = p2->ch[iP2];
-			iC ++;
-			iP2 ++;
-		}
-	}
+    int iC = 0, iP2 = 0;
+    while (iP2 < N) {
+        if(duplicated(p2->ch[iP2], p1->ch, s1, s2)) {
+            iP2 ++;
+        } else {
+            c->ch[iC] = p2->ch[iP2];
+            iC ++;
+            iP2 ++;
+        }
+    }
 }
 void directFromFirst(const SOL *p1, const SOL *p2, SOL *c) {
-	for (int i = 0; i < N; i++) {
-		c->ch[i] = p1->ch[i];
-	}
+    for (int i = 0; i < N; i++) {
+        c->ch[i] = p1->ch[i];
+    }
 }
 void crossover(const SOL *p1, const SOL *p2, SOL *c) {
   OrderCrossover(p1, p2, c);
@@ -197,21 +197,21 @@ void crossover(const SOL *p1, const SOL *p2, SOL *c) {
 // mutate the solution s
 // currently this operator does nothing
 void inversionMutation(SOL *s) {
-	setRandomSliceIndexes();
-	int i, temp;
-	//printf("slice from %d to %d\n", s1, s2);
-	//pprint("before", s);
-	for(i = 0; i < (s2 - s1) / 2; i ++) {
-		//printf("[%d<->%d]", s1+i, s2-i);
-		temp = s->ch[s1+i];
-		s->ch[s1+i] = s->ch[s2-i];
-		s->ch[s2-i] = temp;
-	}
-	//pprint("after", s);
+    setRandomSliceIndexes();
+    int i, temp;
+    //printf("slice from %d to %d\n", s1, s2);
+    //pprint("before", s);
+    for(i = 0; i < (s2 - s1) / 2; i ++) {
+        //printf("[%d<->%d]", s1+i, s2-i);
+        temp = s->ch[s1+i];
+        s->ch[s1+i] = s->ch[s2-i];
+        s->ch[s2-i] = temp;
+    }
+    //pprint("after", s);
 }
 void mutation(SOL *s) {
-	inversionMutation(s);
-	eval(s);
+    inversionMutation(s);
+    eval(s);
 }
 
 
@@ -229,84 +229,84 @@ int getIndexOfWorst() {
   } return index;
 }
 void replacement(const SOL *offspr) {
-	int i, p = getIndexOfWorst();
-	population[p].f = offspr->f;
-	for (i = 0; i < N; i++) {
-		population[p].ch[i] = offspr->ch[i];
-	}
+    int i, p = getIndexOfWorst();
+    population[p].f = offspr->f;
+    for (i = 0; i < N; i++) {
+        population[p].ch[i] = offspr->ch[i];
+    }
 }
 
 int GENERATION;
 // a "steady-state" GA
 void GA() {
-	int i;
-	SOL c;
-	time_t begin = time(NULL);
+    int i;
+    SOL c;
+    time_t begin = time(NULL);
 
-	for (i = 0; i < PSIZE; i++) {
-		gen_rand_sol(&population[i]);
-	}
+    for (i = 0; i < PSIZE; i++) {
+        gen_rand_sol(&population[i]);
+    }
 
-	while (1) {
-		if(time(NULL) - begin >= TimeLimit - 1) return; // end condition
-		SOL *p1, *p2;
-		selection(&p1); selection(&p2);
-		crossover(p1, p2, &c);
-		mutation(&c);
-		replacement(&c);
-		GENERATION ++;
-	}
+    while (1) {
+        if(time(NULL) - begin >= TimeLimit - 1) return; // end condition
+        SOL *p1, *p2;
+        selection(&p1); selection(&p2);
+        crossover(p1, p2, &c);
+        mutation(&c);
+        replacement(&c);
+        GENERATION ++;
+    }
 }
 
 
 // read the test case from stdin
 // and initialize some values such as record.f and Dist
 void init() {
-	FILE *pf = fopen("../input/cycle.in.101", "r");
-	int i, j, tmp;
-	double time_limit;
+    FILE *pf = fopen("../input/cycle.in.101", "r");
+    int i, j, tmp;
+    double time_limit;
 
-	tmp = fscanf(pf, "%d", &N);
-	for (i = 0; i < N; i++) {
-		tmp = fscanf(pf, "%lf %lf", &X[i], &Y[i]);
-	}
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			double dx = X[i] - X[j], dy = Y[i] - Y[j];
-			Dist[i][j] = sqrt(dx*dx + dy*dy);
-		}
-	}
-	tmp = fscanf(pf, "%lf", &time_limit);
-	TimeLimit = (long long) time_limit;
+    tmp = fscanf(pf, "%d", &N);
+    for (i = 0; i < N; i++) {
+        tmp = fscanf(pf, "%lf %lf", &X[i], &Y[i]);
+    }
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            double dx = X[i] - X[j], dy = Y[i] - Y[j];
+            Dist[i][j] = sqrt(dx*dx + dy*dy);
+        }
+    }
+    tmp = fscanf(pf, "%lf", &time_limit);
+    TimeLimit = (long long) time_limit;
 
-	record.f = 1e100;
-	GENERATION = 0;
+    record.f = 1e100;
+    GENERATION = 0;
 }
 
 
 // print the best solution found to stdout
 void answer() {
-	int i;
+    int i;
 
-	for (i = 0; i < N; i++) {
-		if (i > 0) printf(" ");
-		printf("%d", record.ch[i]+1);
-	}
-	printf(" %lf", record.f);
-	printf(" GENERATION : %d", GENERATION);
-	printf("\n");
+    for (i = 0; i < N; i++) {
+        if (i > 0) printf(" ");
+        printf("%d", record.ch[i]+1);
+    }
+    printf(" %lf", record.f);
+    printf(" GENERATION : %d", GENERATION);
+    printf("\n");
 }
 
 
 int main() {
-	srand(time(NULL));
+    srand(time(NULL));
   int nLoop = 29;
   for(int loop = 0; loop < nLoop; loop++) {
-  	init();
-  	GA();
-  	answer();
+    init();
+    GA();
+    answer();
   }
-	return 0;
+    return 0;
 }
 
 
